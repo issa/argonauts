0538903c22f999a839c5d4eb8a060687b253c4e3
Issa <issa@luniki.de>
2016-07-16 12:24:07 +0200

    scaffolding

---
+++ ArgonautsPlugin.class.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * ArgonautsPlugin.class.php
+ *
+ * ...
+ *
+ * @author  Issa <issa@luniki.de>
+ * @version 0.1.0
+ */
+
+class ArgonautsPlugin extends StudIPPlugin implements SystemPlugin {
+
+    public function __construct() {
+        parent::__construct();
+    }
+}
