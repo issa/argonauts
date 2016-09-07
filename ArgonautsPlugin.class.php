ae4839b856bd7cd538e1743aa00256129ce33495
Issa <issa@luniki.de>
2016-09-07 09:09:06 +0200

    add to nobody role by default

--- ArgonautsPlugin.class.php
+++ ArgonautsPlugin.class.php
@@ -9,9 +9,20 @@
  * @version 0.1.0
  */
 
-class ArgonautsPlugin extends StudIPPlugin implements SystemPlugin {
+class ArgonautsPlugin extends \StudIPPlugin implements \SystemPlugin {
 
     public function __construct() {
         parent::__construct();
     }
+
+    static function onEnable($id)
+    {
+        // enable nobody role by default
+        \RolePersistence::assignPluginRoles($id, array(7));
+    }
+
+    static function onDisable($id)
+    {
+    }
+
 }
