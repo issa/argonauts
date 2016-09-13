cb4048444aa287fe73ef06972723b246a7af641e
Issa <issa@luniki.de>
2016-09-13 10:37:13 +0200

    differentiate between un/authorized routes

--- lib/JsonApi.php
+++ lib/JsonApi.php
@@ -15,13 +15,34 @@ class JsonApi {
 
     public function __invoke()
     {
-        $this->app->get('', function (Request $request, Response $response, $args) {
-            $response->getBody()->write("Hello, dummy from inside of " . __CLASS__);
-            return $response;
-        });
+        // authorized
+        $this->app
+            ->group('', [$this, 'authorizedRoutes'])
+            ->add(function ($request, $response, $next) {
+                // TODO: authorize me here
+                return $next($request, $response);
+            });
 
+        // unauthorized
+        $this->app->group('', [$this, 'unauthorizedRoutes']);
 
         // Register plugin routes
         \PluginEngine::sendMessage('Argonauts\\JsonApiPlugin', 'registerRoutes', $this->app);
+    }
+
+    public function authorizedRoutes()
+    {
+        $this->app->get('/auth', function (Request $request, Response $response, $args) {
+            $response->getBody()->write("Hello, authorized user, from inside of " . __CLASS__);
+            return $response;
+        });
+    }
+
+    public function unauthorizedRoutes()
+    {
+        $this->app->get('/unauth', function (Request $request, Response $response, $args) {
+            $response->getBody()->write("Hello, unauthorized user, from inside of " . __CLASS__);
+            return $response;
+        });
     }
 }
